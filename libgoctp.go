843ead9de2bb358f4c04f4dd1d3a301b1a2e1719
try876 <small_tank@qq.com>
2019-05-16 16:54:53 +0800

    穿透式api 6.3.13 评测版，编译方法：1、运行 swig -go -cgo -c++ -intgosize 64 goctp.swigcxx 生成goctp.go 2 修改 goctp.go中的单引号问题 3 将goctp.swigcxx改后缀 4 make build

--- libgoctp.go
+++ libgoctp.go
@@ -7,8 +7,8 @@
 package goctp
 
 /*
-#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/SFIT_CTP_6.3.11_20180109_tradeapi/v6.3.11_20180109_api_tradeapi_linux64 -Wl,-rpath,${SRCDIR}/api/SFIT_CTP_6.3.11_20180109_tradeapi/v6.3.11_20180109_api_tradeapi_linux64 -lthostmduserapi -lthosttraderapi -lstdc++
-#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/SFIT_CTP_6.3.11_20180109_tradeapi/v6.3.11_20180109_api_tradeapi_linux64
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/linux64 -Wl,-rpath,${SRCDIR}/api/linux64 -lLinuxDataCollect -lthostmduserapi_se -lthosttraderapi_se -lstdc++
+#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/linux64
 #cgo windows LDFLAGS: -fPIC -L${SRCDIR}/api/SFIT_CTP_6.3.11_20180109_tradeapi/20180109_tradeapi64_windows -Wl,-rpath,${SRCDIR}/api/SFIT_CTP_6.3.11_20180109_tradeapi/20180109_tradeapi64_windows ${SRCDIR}/api/SFIT_CTP_6.3.11_20180109_tradeapi/20180109_tradeapi64_windows/thostmduserapi.lib ${SRCDIR}/api/SFIT_CTP_6.3.11_20180109_tradeapi/20180109_tradeapi64_windows/thosttraderapi.lib -lthostmduserapi -lthosttraderapi
 #cgo windows CPPFLAGS: -fPIC -I${SRCDIR}/api/SFIT_CTP_6.3.11_20180109_tradeapi/20180109_tradeapi64_windows -DISLIB -DWIN32 -DLIB_MD_API_EXPORT -DLIB_TRADER_API_EXPORT
 */
